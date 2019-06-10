aa543e37bdc867eada430e42652920b37f511acf
Vandana Ganesh <vandana.ganesh@hcl.com>
2019-06-10 15:26:20 -0500

    test commit

--- oms/src/app/login/login.component.ts
+++ oms/src/app/login/login.component.ts
@@ -7,12 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  inputUsername;
-  inputPassword;
-  
   constructor() { }
 
   ngOnInit() {
+ 
   }
 
 }
--- oms/src/app/verify-login.service.ts
+++ oms/src/app/verify-login.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
 @Injectable({
   providedIn: 'root'
 })
 export class VerifyLoginService {
 
+  
   constructor() { }
+
+  public boolean tryLogin(userInfo: User) {
+
+  }
 }
