615aed3f81a6894340a2fef2489cac0c0abf3251
bmalouf <brian.malouf@gmail.com>
2019-06-10 13:57:28 -0500

    created components for login, order, ordercreate. added routing.

--- oms/src/app/app.module.ts
+++ oms/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { OrderComponent } from './order/order.component';
+import { OrderCreateComponent } from './order-create/order-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    OrderComponent,
+    OrderCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent },
+      { path: 'order', component: OrderComponent },
+      { path: 'order/create', component: OrderCreateComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
