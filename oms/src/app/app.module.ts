42f1664986150372f1b59ecdd59d0d96eacd4461
bmalouf <brian.malouf@gmail.com>
2019-06-11 10:20:35 -0500

    updating module imports for forms

--- oms/src/app/app.module.ts
+++ oms/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { OrderComponent } from './order/order.component';
 import { OrderCreateComponent } from './order-create/order-create.component';
@@ -17,6 +18,8 @@ import { OrderCreateComponent } from './order-create/order-create.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: LoginComponent },
       { path: 'order', component: OrderComponent },
