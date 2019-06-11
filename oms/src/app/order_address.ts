bff43484b4519f59b71edca785d08492bb7a98bc
Abhishek Patil <patil.abhishek150@gmail.com>
2019-06-11 12:44:19 -0500

    Create Order Component classes

---
+++ oms/src/app/order_address.ts
@@ -0,0 +1,9 @@
+export class Order_Address{
+  constructor(
+    public address1		: string,
+    public address2		: string,
+    public state		: string,
+    public city		: string,
+    public zip		: number
+){}
+};
