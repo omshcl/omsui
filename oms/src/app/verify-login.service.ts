64bb5fc33bd7fc2923868b2ae768d3116ca349f5
Vandana Ganesh <vandana.ganesh@hcl.com>
2019-06-11 09:31:06 -0500

    took out error in verify login and worked on validating username/passwork

--- oms/src/app/verify-login.service.ts
+++ oms/src/app/verify-login.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {User} from './user'
 @Injectable({
   providedIn: 'root'
 })
@@ -7,7 +8,7 @@ export class VerifyLoginService {
   
   constructor() { }
 
-  public boolean tryLogin(userInfo: User) {
-
+  public tryLogin(userInfo: User) {
+      
   }
 }
