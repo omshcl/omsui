dbcf494a67abf25bc9716d68657e68aa04e9986d
Abhishek Patil <patil.abhishek150@gmail.com>
2019-06-12 09:45:28 -0500

    Revert "Order Create Component template"
    This reverts commit 46144087464c50642e168e43e2021a9dc9f80982.

--- oms/src/app/app.module.ts
+++ oms/src/app/app.module.ts
@@ -3,13 +3,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material';
 import { LoginComponent } from './login/login.component';
 import { OrderComponent } from './order/order.component';
 import { OrderCreateComponent } from './order-create/order-create.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -21,16 +17,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
     RouterModule.forRoot([
       { path: '', component: LoginComponent },
       { path: 'order', component: OrderComponent },
       { path: 'order/create', component: OrderCreateComponent },
-    ]),
-    BrowserAnimationsModule
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- oms/src/app/order-create/order-create.component.ts
+++ oms/src/app/order-create/order-create.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Order } from '../order';
-import { Order_Address } from '../order_address';
 
 @Component({
   selector: 'app-order-create',
@@ -9,19 +7,9 @@ import { Order_Address } from '../order_address';
 })
 export class OrderCreateComponent implements OnInit {
 
-  items = ["item1", "item2", "item3"];
-  costs = ["1", "2", "3", "4"];
-
-  order_address = new Order_Address('1234 Main St', 'Apt 2825', 'Tx', 'Richardson', 752525)
-  orderModel = new Order('item1', 'new item', 5, 2, 50, this.order_address)
-
   constructor() { }
 
   ngOnInit() {
-  }
-
-  onSubmit(){
-    console.log(this.orderModel)
   }
 
 }
