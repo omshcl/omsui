a7d65d9ba0ccbf906064f3e3ee174af9b6c55f41
Vandana Ganesh <vandana.ganesh@hcl.com>
2019-06-13 15:54:33 -0500

    backend login verification

---
+++ oms/src/app/verify-login.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+const httpOptions = {
+  headers: new HttpHeaders({ "Content-Type": "application/json" })
+};
+
+@Injectable({
+  providedIn: "root"
+})
+export class VerifyLoginService {
+  apiURL: string = "http://3ab08cc9.ngrok.io/login";
+  constructor(private http: HttpClient) {}
+
+  verifyBackend(userL) {
+    return this.http
+      .post(this.apiURL, {
+        username: userL.value.user,
+        password: userL.value.yes
+      })
+      .subscribe(
+        data => {
+          if(data.isValid && data.isAdmin) {
+            location.href = "./order"
+          }
+          else if(data.isValid && !data.isAdmin) {
+            location.href = "./order-agent"
+          }
+          else {
+                 alert("Invalid Login. Check credentials.")
+               }
+          return true;
+        },
+        error => {
+          console.error("Error posting order!");
+          return false;
+        }
+      );
+  }
+}
