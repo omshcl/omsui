94cc4d5269cba0013cd3f7b0d53fbf5072b9bf76
Vandana Ganesh <vandana.ganesh@hcl.com>
2019-06-19 11:04:19 -0500

    removed refresh

--- oms/src/app/components/order-create/order-create.component.ts
+++ oms/src/app/components/order-create/order-create.component.ts
@@ -155,7 +155,6 @@ export class OrderCreateComponent implements OnInit {
 
   processedOrder() {
     alert("Order has been placed");
-    location.href = "/order/create";
   }
 
   setOrderFormValue(field, value) {
