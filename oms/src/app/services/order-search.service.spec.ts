684d25fdb8a2a99c8541988f35dd6813c10378e7
Abhishek Patil <patil.abhishek150@gmail.com>
2019-06-25 11:56:21 -0500

    .spec file changes

--- oms/src/app/services/order-search.service.spec.ts
+++ oms/src/app/services/order-search.service.spec.ts
@@ -1,11 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { OrderSearchService } from './order-search.service';
+import { OrderSearchService } from "./order-search.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
-describe('OrderSearchService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe("OrderSearchService", () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    })
+  );
 
-  it('should be created', () => {
+  it("should be created", () => {
     const service: OrderSearchService = TestBed.get(OrderSearchService);
     expect(service).toBeTruthy();
   });
