c95e4d1260e5b9bba20f0eb3807788659205a7b6
Abhishek Patil <patil.abhishek150@gmail.com>
2019-06-25 11:08:53 -0500

    .spec file changes

--- oms/src/app/components/orders/order-search/order-search.component.spec.ts
+++ oms/src/app/components/orders/order-search/order-search.component.spec.ts
@@ -1,16 +1,32 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ReactiveFormsModule } from "@angular/forms";
+import { OrderSearchComponent } from "./order-search.component";
+import { OrderComponent } from "../order/order.component";
+import { Component } from "@angular/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from "@angular/material";
+import { MatTableModule, MatFormFieldModule } from "@angular/material";
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from "@angular/common/http/testing";
 
-import { OrderSearchComponent } from './order-search.component';
-
-describe('OrderSearchComponent', () => {
+describe("OrderSearchComponent", () => {
   let component: OrderSearchComponent;
   let fixture: ComponentFixture<OrderSearchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OrderSearchComponent ]
-    })
-    .compileComponents();
+      declarations: [OrderSearchComponent, OrderComponent],
+      imports: [
+        ReactiveFormsModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatTableModule,
+        MatFormFieldModule,
+        HttpClientTestingModule
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +35,7 @@ describe('OrderSearchComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
