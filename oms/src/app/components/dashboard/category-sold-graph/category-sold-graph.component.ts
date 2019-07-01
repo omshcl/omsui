dbb41661e4eb1427aedb9c852576083a9a3213c1
bmalouf <brian.malouf@gmail.com>
2019-07-01 12:35:39 -0500

    default dashboard componenent created

---
+++ oms/src/app/components/dashboard/category-sold-graph/category-sold-graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-sold-graph',
+  templateUrl: './category-sold-graph.component.html',
+  styleUrls: ['./category-sold-graph.component.css']
+})
+export class CategorySoldGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
