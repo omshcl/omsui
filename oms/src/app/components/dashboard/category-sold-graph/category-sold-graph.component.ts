bd4c49e350c9cae39e69b1b50d59342fa5a005b0
bmalouf <brian.malouf@gmail.com>
2019-07-01 12:50:00 -0500

    updated order search to have fullfill button

--- oms/src/app/components/dashboard/category-sold-graph/category-sold-graph.component.ts
+++ oms/src/app/components/dashboard/category-sold-graph/category-sold-graph.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Chart } from "chart.js";
+import { OrderSearchService } from "src/app/services/order-search.service";
 
 @Component({
-  selector: 'app-category-sold-graph',
-  templateUrl: './category-sold-graph.component.html',
-  styleUrls: ['./category-sold-graph.component.css']
+  selector: "app-category-sold-graph",
+  templateUrl: "./category-sold-graph.component.html",
+  styleUrls: ["./category-sold-graph.component.css"]
 })
 export class CategorySoldGraphComponent implements OnInit {
+  dataResponse;
+  chart = [];
 
-  constructor() { }
+  constructor(private _OrderSearchService: OrderSearchService) {}
 
   ngOnInit() {
+    this._OrderSearchService.getOrders().subscribe(response => {
+      this.dataResponse = response;
+      console.log(this.dataResponse);
+    });
   }
-
 }
