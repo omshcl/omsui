db1ce373477bf2b1dd0495018872f66492b2c16e
Shivaprakash Sivagurunathan <s.shivaprakash29@gmail.com>
2019-07-08 10:13:01 -0700

    Update admin.guard.spec.ts

--- oms/src/app/admin.guard.spec.ts
+++ oms/src/app/admin.guard.spec.ts
@@ -2,11 +2,14 @@ import { TestBed, async, inject } from "@angular/core/testing";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { AdminGuard } from "./admin.guard";
 import { RouterModule } from "@angular/router";
-
+import {APP_BASE_HREF} from '@angular/common';
 describe("AdminGuard", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AdminGuard],
+     
+      providers: [AdminGuard,
+        { provide: APP_BASE_HREF, useValue : '/' }
+      ],
       imports: [HttpClientTestingModule, RouterModule.forRoot([])]
     });
   });
